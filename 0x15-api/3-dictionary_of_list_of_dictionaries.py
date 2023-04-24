5c28b9be87be9e57f16ab26f0fb950aeb9deca76
malik <mlanlokun@gmail.com>
2023-04-24 21:00:19 +0200

    dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Dictionary of list of dictionaries"""
-
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
-from sys import argv
-
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
-
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({user.get("id"): [{
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": user.get("username")
-        } for task in requests.get(url + "todos",
-                                      params={"userId": user.get("id")}).json()]
-
-        for user in users}, jsonfile)
-        
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
