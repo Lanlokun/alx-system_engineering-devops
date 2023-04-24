f16f5b3ff7b328827188a0c429d7e30e5c0d0a62
malik <mlanlokun@gmail.com>
2023-04-24 20:48:15 +0200

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Export to CSV """
+
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """ For a given employee ID, returns information about his/her TODO list
+    progress """
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todo = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            writer.writerow([user_id, user.get("username"),
+                             task.get("completed"), task.get("title")])
+    
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,3 +1,12 @@
 # 0x15. API
 
 # 0. Gather data from an API
+
+
+# 1. Export to CSV
+
+
+# 2. Export to JSON
+
+
+# 3. Dictionary of list of dictionaries
