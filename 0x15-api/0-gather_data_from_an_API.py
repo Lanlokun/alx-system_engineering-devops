9f7d4e73c9541a7b787347e554252c78c1359088
malik <mlanlokun@gmail.com>
2023-04-26 19:49:11 +0200

    fixed employee name error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,13 +13,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     todo = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    completed_tasks = []
-    for task in todo:
-        if task.get("completed") is True:
-            completed_tasks.append(task.get("title"))
-
+    completed = [t.get("title") for t in todo if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed_tasks), len(todo)))
-
-    for task in completed_tasks:
-        print("\t {}".format(task))
+        user.get("name"), len(completed), len(todo)))
+    [print("\t {}".format(c)) for c in completed]
