582255b94aee3cbe846289b8afdca6130c65145d
malik <mlanlokun@gmail.com>
2023-05-09 16:35:49 +0300

    updates

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,8 +6,8 @@ import requests
 
 
 def count_words(subreddit, word_list, after="", word_dict={}):
-    """ returns a list containing the titles of all hot articles for a given
-    subreddit."""
+    """ parses the title of all hot articles, and prints a sorted count of
+    given keywords """
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
                                                                  after)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
@@ -16,20 +16,21 @@ def count_words(subreddit, word_list, after="", word_dict={}):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         for post in response.json().get("data").get("children"):
+            title = post.get("data").get("title").lower().split()
             for word in word_list:
-                if word.lower() in post.get("data").get("title").lower():
-                    if word.lower() not in word_dict.keys():
-                        word_dict[word.lower()] = 1
+                if word.lower() in title:
+                    if word in word_dict:
+                        word_dict[word] += 1
                     else:
-                        word_dict[word.lower()] += 1
+                        word_dict[word] = 1
         after = response.json().get("data").get("after")
         if after is None:
             if len(word_dict) == 0:
-                return
+                return None
             for key, value in sorted(word_dict.items(),
                                      key=lambda x: (-x[1], x[0])):
                 print("{}: {}".format(key, value))
             return
         return count_words(subreddit, word_list, after, word_dict)
     else:
-        return None
+        return
